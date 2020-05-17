f8e033425b9c05f4c40490c52704a700a47bf16b
Andrei Dobrescu <usingvirtualbox@gmail.com>
2020-05-17 15:40:12 +0300

    refactorings

--- dyndao/src/main/java/ro/dobrescuandrei/roomdynamicdao/QueryJoinClauses.kt
+++ dyndao/src/main/java/ro/dobrescuandrei/roomdynamicdao/QueryJoinClauses.kt
@@ -15,10 +15,37 @@ class QueryJoinClauses : LinkedList<String>()
         add(" $type join $remoteTable on $table.$column = $remoteTable.$remoteColumn")
     }
 
+    fun addInnerJoin      (builder : JoinClauseBuilder) = join("inner",       builder)
+    fun addCrossJoin      (builder : JoinClauseBuilder) = join("cross",       builder)
+    fun addLeftOuterJoin  (builder : JoinClauseBuilder) = join("left outer",  builder)
+    fun addRightOuterJoin (builder : JoinClauseBuilder) = join("right outer", builder)
+    fun addFullOuterJoin  (builder : JoinClauseBuilder) = join("full outer",  builder)
+
+    private fun join(type : String, builder : JoinClauseBuilder)
+    {
+        join(type = type, remoteTable = builder.remoteTable, remoteColumn = builder.remoteColumn,
+            table = builder.table, column = builder.column)
+    }
+
     fun merge() : String?
     {
         if (isNotEmpty())
             return this.joinToString(separator = " ")
         return null
     }
+}
+
+class JoinClauseBuilder
+{
+    internal lateinit var remoteTable : String
+    fun remoteTable(remoteTable : String) = also { this.remoteTable=remoteTable }
+
+    internal lateinit var remoteColumn : String
+    fun remoteColumn(remoteColumn : String) = also { this.remoteColumn=remoteColumn }
+
+    internal lateinit var table : String
+    fun table(table : String) = also { this.table=table }
+
+    internal lateinit var column : String
+    fun column(column : String) = also { this.column=column }
 }
--- dyndao/src/main/java/ro/dobrescuandrei/roomdynamicdao/QueryProjectionClauses.kt
+++ dyndao/src/main/java/ro/dobrescuandrei/roomdynamicdao/QueryProjectionClauses.kt
@@ -16,10 +16,27 @@ class QueryProjectionClauses : LinkedList<String>()
         return this
     }
 
+    fun addField(builder : ProjectionClauseBuilder)
+    {
+        addField(fieldName = builder.fieldName, fromTable = builder.fromTable, projectAs = builder.projectAs)
+    }
+
     fun merge() : String
     {
         if (isNotEmpty())
             return this.joinToString(separator = " , ")
         return "*"
     }
+}
+
+class ProjectionClauseBuilder
+{
+    internal lateinit var fieldName : String
+    fun fieldName(fieldName : String) = also { this.fieldName=fieldName }
+
+    internal lateinit var fromTable : String
+    fun fromTable(fromTable : String) = also { this.fromTable=fromTable }
+
+    internal lateinit var projectAs : String
+    fun projectAs(projectAs : String) = also { this.projectAs=projectAs }
 }
